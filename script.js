101dc6c58cd76f1766f598a766f48beed7aaaf66
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-08 00:09:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+	/ Remove non-alphanumeric characters and convert to lowercase
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    // Compare the string with its reverse
+    return s === s.split('').reverse().join('');
 
 }
 module.exports = palindrome
